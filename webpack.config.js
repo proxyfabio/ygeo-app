2b299cae86f2441977badb3737c65c92c3dd2b65
Sergo <rogaldh@radsh.red>
2015-04-23 12:46:45 +0300

    Switch babel to the runtime mode

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     loaders: [{
       test: /\.js.*$/,
       exclude: /node_modules/,
-      loaders: ['babel-loader']
+      loaders: ['babel-loader?optional=runtime']
     }, {
       test: /\.(png)$/,
       loader: 'url?name=[name].[ext]'
